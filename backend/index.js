acc48dab10cced03eb0a835a66b03ed32b2a7ae1
lakshay17 <lakshayjain01lj@gmail.com>
2025-01-08 21:19:25 +0530

    Implemented backend logics

--- backend/index.js
+++ backend/index.js
@@ -1,39 +1,64 @@
 const express = require("express")
 const app = express()
 const port = 3000
-const { createTodo, updateTodo} = require('./types');
-
+const { createTodo, updateTodo } = require('./types');
+const { todo } = require('./db')
 
 app.use(express.json())
 
 
-app.post("/todo", (req,res)=>{
-    const createPayload = req.body;
-    const parsedPayload = createTodo.safeParse(createPayload)
-    
-    if (!parsedPayload.success){
-      res.status(411).json({
-        msg :  "You sent the wrong inputs"
-      })
-      return
-    }
+app.post("/todo", async (req, res) => {
+  const createPayload = req.body;
+  const parsedPayload = createTodo.safeParse(createPayload)
+
+  if (!parsedPayload.success) {
+    res.status(411).json({
+      msg: "You sent the wrong inputs"
+    })
+    return
+  }
+
+  await todo.create({
+    title: createPayload.title,
+    description: createPayload.description,
+    completed: false
+  })
+
+  res.json({
+    msg: "Todo created successfully"
+  })
+
 })
 
-app.get("/todos", (req,res)=>{
+app.get("/todos", async (req, res) => {
+  const todos = await todo.find()
 
+  res.json({
+    msg: "Todos fetched successfully",
+  })
 })
 
-app.put("/completed" ,(req,res)=>{
-    const updatePayload = req.body;
-    const parsedPayload = updateTodo.safeParse(updatePayload)
+app.put("/completed", async (req, res) => {
+  const updatePayload = req.body;
+  const parsedPayload = updateTodo.safeParse(updatePayload)
 
-    if (!parsedPayload.success){
-      res.status(411).json({
-        msg :  "You sent the wrong inputs"
-      })
-      return
-    }
-    
+  if (!parsedPayload.success) {
+    res.status(411).json({
+      msg: "You sent the wrong inputs"
+    })
+    return
+  }
+
+  await todo.update({
+    _id: req.body.id
+  }, {
+    completed: true
+  })
+
+  res.json({
+    msg : "Todo marked as completed"
+  })
+
 })
 
 app.listen(port, () => {

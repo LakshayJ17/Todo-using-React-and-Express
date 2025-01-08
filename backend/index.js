969818bae6c2350b6e5a3c81366f41a2db23c31e
lakshay17 <lakshayjain01lj@gmail.com>
2025-01-08 20:51:03 +0530

    implemented zod

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,22 @@
 const express = require("express")
 const app = express()
 const port = 3000
+const { createTodo, updateTodo} = require('./types');
+
 
 app.use(express.json())
 
 
 app.post("/todo", (req,res)=>{
-
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload)
+    
+    if (!parsedPayload.success){
+      res.status(411).json({
+        msg :  "You sent the wrong inputs"
+      })
+      return
+    }
 })
 
 app.get("/todos", (req,res)=>{
@@ -14,7 +24,16 @@ app.get("/todos", (req,res)=>{
 })
 
 app.put("/completed" ,(req,res)=>{
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload)
 
+    if (!parsedPayload.success){
+      res.status(411).json({
+        msg :  "You sent the wrong inputs"
+      })
+      return
+    }
+    
 })
 
 app.listen(port, () => {
